637f51f8ae2a61d2d482e25be90d3136673ed545
kelmorgan <kelmorgan18@gmail.com>
2022-04-05 10:57:56 +0100

    first commit

---
+++ src/main/java/com/kelmorgan/myreactivespring/MyreactiveSpringApplication.java
@@ -0,0 +1,13 @@
+package com.kelmorgan.myreactivespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyreactiveSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyreactiveSpringApplication.class, args);
+	}
+
+}
